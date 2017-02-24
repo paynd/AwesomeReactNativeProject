bf7f1e4afb9349046099f22c40ad70fba13c4e3f
paynd <mrpaynd@gmail.com>
2017-02-24 13:59:02 +0300

    strange props

--- src/ui/Blink.js
+++ src/ui/Blink.js
@@ -2,15 +2,21 @@
  * Created by paynd on 21.02.17.
  */
 
-import React, { Component } from 'react'
-import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+import React, { Component, PropTypes } from 'react'
+import { Text } from 'react-native'
 
 export default class Blink extends Component {
+  static propTypes = {
+    children: PropTypes.string,
+  }
+
   constructor(props) {
     super(props)
-    this.state = { showText: true }
-    this.state.textValue = this.props.children
-    // Toggle the state every second
+    this.state = { showText: true,
+      textValue: this.props.children,
+    }
+    // this.state.textValue = this.props.children
+     // Toggle the state every second
     setInterval(() => {
       this.setState({ showText: !this.state.showText })
     }, 1000)

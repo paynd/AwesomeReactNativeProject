7b11c00ef888edbc30f0a2f1cdca954f71f62312
paynd <mrpaynd@gmail.com>
2017-02-24 21:28:06 +0300

    listview works, rowitem extracted

--- src/ui/ImageListView.js
+++ src/ui/ImageListView.js
@@ -1,9 +1,10 @@
 /**
  * Created by paynd on 22.02.17.
  */
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 import { ListView, Text, View } from 'react-native'
 import getListOfImages from '../util/Network'
+import RowItemRenderer from './RowItem'
 
 export default class ImageListView extends Component {
   state = {
@@ -15,16 +16,8 @@ export default class ImageListView extends Component {
   }
 
   onSuccess = (response) => {
-    console.log(` responce typeof ${typeof response}`)
+    // console.log(` response typeof ${typeof response}`)
     const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
-
-    // if (response.status !== 200) {
-    //     console.log('### Status Code: ' + response.status);
-    //     this.setState({
-    //         isData: false
-    //     });
-    //     return;
-    // }
 
     console.log('### Ok')
 
@@ -48,36 +41,18 @@ export default class ImageListView extends Component {
     if (this.state.isData === false) {
       return <Text>No data loaded</Text>
     }
-
     return (
       <ListView
         dataSource={this.state.dataSource}
-        renderRow={rowData => <RowItem rowData={rowData} />}
+        renderRow={data => <RowItemRenderer {...data} />}
       />
     )
   }
 
   render() {
     return (
-      <View style={{ flex: 1, paddingTop: 22 }}>
+      <View style={{ flex: 10, paddingTop: 22 }}>
         {this.renderList()}
-      </View>
-    )
-  }
-}
-
-class RowItem extends Component {
-  static propTypes = {
-    rowData: PropTypes.object.isRequired,
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1, paddingTop: 22 }}>
-        <Image
-          source={{ uri: this.props.rowData.thumbnailUrl }}
-          style={{ width: 150, height: 150 }}
-        />
       </View>
     )
   }
